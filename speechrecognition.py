40856ab6666e79742580121e7f353e89d510982a
Nico Hahn <nicohahn@live.de>
2020-01-27 17:32:06 +0100

    read forbidden words from database

--- speechrecognition.py
+++ speechrecognition.py
@@ -3,8 +3,35 @@
 # on mac also install:
 # brew install portaudio & pip install --global-option='build_ext' --global-option='-I/usr/local/include' --global-option='-L/usr/local/lib' pyaudio
 import speech_recognition as sr
+import mysql.connector
+from mysql.connector import Error
 
-swearwords = ['fuck', 'penis', 'hello']
+swearwords = []
+
+def getSwearWords(result):
+    for item in result:
+        swearwords.append(item[0])
+    for word in swearwords:
+        print(word)
+    
+try:
+    connection = mysql.connector.connect(host='127.0.0.1', database='swearstopper', user='root', password='root')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor(buffered=True)
+        query = cursor.execute("SELECT * FROM forbidden_words")
+        result = cursor.fetchall()
+        getSwearWords(result)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
+
 r = sr.Recognizer()
 while True:
     with sr.Microphone() as source:
