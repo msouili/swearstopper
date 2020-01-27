24dd95aab58f2bb044ed6773220f8bfb89e3a63c
rhlxvr <marvin.wagle@icloud.com>
2020-01-27 11:15:57 +0100

    checks whether there are swearwords in the sentence

--- speechrecognition.py
+++ speechrecognition.py
@@ -7,12 +7,12 @@ import speech_recognition as sr
 swearwords = ['fuck', 'penis', 'hello']
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print ('Say Something!')
+    print('Say Something!')
     audio = r.listen(source)
-    print ('Done!')
+    print('Done!')
     
 text = r.recognize_google(audio)
-print (text)
+print(text)
 
 if any(word in text for word in swearwords):
-    print ('stop swearing!')
+    print('stop swearing!')
