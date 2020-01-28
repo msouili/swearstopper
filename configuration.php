f35c83f861747dcc508232c3ae7b2ca588cf3950
muffinPSD <marvin.volkmann.mv@gmail.com>
2020-01-28 09:59:40 +0100

    write words to db

--- configuration.php
+++ configuration.php
@@ -1,18 +1,16 @@
 <?php
-    // die Konstanten auslagern in eigene Datei
-    // die dann per require_once ('konfiguration.php'); 
-    // geladen wird.
-    
-    // Damit alle Fehler angezeigt werden
-    error_reporting(E_ALL);
-    
-    // Zum Aufbau der Verbindung zur Datenbank
-    // die Daten erhalten Sie von Ihrem Provider
-    define ( 'MYSQL_HOST',      '127.0.0.1' );
-    
-    // bei XAMPP ist der MYSQL_Benutzer: root
-    define ( 'MYSQL_BENUTZER',  'root' );
-    define ( 'MYSQL_KENNWORT',  'root' );
-    // für unser Bsp. nennen wir die DB adressverwaltung
-    define ( 'MYSQL_DATENBANK', 'swearstopper' );
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "swearstopper";
+
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,77 @@
 <?php
-    # include 'index.html';
-    require_once ('configuration.php');
-    $db_link = mysqli_connect (
-                        MYSQL_HOST, 
-                        MYSQL_BENUTZER, 
-                        MYSQL_KENNWORT, 
-                        MYSQL_DATENBANK
-                        );
-    if ( $db_link )
-    {
-        echo 'Verbindung erfolgreich: ';
-        print_r( $db_link);
+require_once ('configuration.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="CSS/main.css">
+        <script src="script.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <title>SwearStopper</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div id="cont" class="container">
+                <div id="" class="row" style="margin-top: 5%">
+                    <form method="POST">
+                        <h2>Schlagwörter</h2>
+                        <input name="title" type="input" placeholde="Schlagwort eingeben">
+                        <!--textarea id="title" name="title" placeholder="Schlagwort" cols="25" rows="1"></textarea-->
+                        <input id="send" class="button-primary" type="submit" value="Absenden">
+                    </form>
+                    <div>
+                        <?php #outputDatabase();?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
+<?php
+
+
+
+
+
+if (isset($_POST["title"])){
+    
+    //query 
+    $input = $_POST["title"];
+    $sqlInsert = "INSERT INTO forbidden_words (forb_word) VALUES ('$input')";
+
+    if ($conn->query($sqlInsert) === TRUE){
+        echo "Neuer Eintrag erfolgreich erstellt <br>";
+    }else{
+        echo "Error: " . $sqlInsert . "<br>" . $conn->error;
     }
-    else
-    {
-        // hier sollte dann später dem Programmierer eine
-        // E-Mail mit dem Problem zukommen gelassen werden
-        die('keine Verbindung möglich: ' . mysqli_error());
-    }
+
+
+}
+
+#function outputDatabase(){
+    $sqlOutput = "SELECT forb_word FROM forbidden_words";
+    $result = $conn->query($sqlOutput);
+
+    if ($result->num_rows > 0){
+        //output data of each row
+        while ($row = $result->fetch_assoc()){
+            echo "Verbotenes Wort: " . $row["forb_word"] . "<br>";
+        }
+    }else{
+            echo "0 Results";
+        }
+    $conn->close();
+#}
+
+
+function debugger($a){
+    echo "<xmp>";
+    print_r($a);
+    echo "</xmp>";
+}
 ?>
